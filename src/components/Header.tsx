1c36cf53e6f8d14899691041c213374d6c8e6965
Milton Velásquez <259344844+VGMil-dev@users.noreply.github.com>
2026-09-23 17:33:22 +0000

    feat(components): Crear Header de la App

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Header = () => {
+  return (
+    <View className='bg-white w-full h-[55px] flex justify-center px-2'> 
+      <Text className="font-bold ">milo_velasquez_99</Text>
+    </View>
+  )
+}
+
+export default Header
